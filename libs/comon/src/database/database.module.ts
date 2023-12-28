1aea209aff43a2c8c544f5e69d312e1ebe0e0037
Ayaf-Bless <blessambel1@gmail.com>
2023-12-28 22:50:30 +0300

    common library

---
+++ libs/comon/src/database/database.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        return {
+          uri: configService.get('MONGODB_URI')
+            ? configService.get('MONGODB_URI')
+            : 'mongodb://127.0.0.1/sleeper',
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
