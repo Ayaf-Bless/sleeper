cb00a903324f240d209413c555e943205d903f67
Ayaf-Bless <blessambel1@gmail.com>
2023-12-29 12:23:22 +0300

    refactoring:remove config and extend it to services

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -3,9 +3,33 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from './user/user.module';
 import { LoggerModule } from '@app/comon';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [UserModule, LoggerModule],
+  imports: [
+    UserModule,
+    LoggerModule,
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: `${configService.get('JWT_EXPIRATION')}s`,
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        JWT_EXPIRATION: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
