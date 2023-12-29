0f666b08ecd7ca3d9ef46163202db2a77b981300
Ayaf-Bless <blessambel1@gmail.com>
2023-12-29 08:13:41 +0300

    add custom logger

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
-import { DatabaseModule } from '@app/comon';
+import { DatabaseModule, LoggerModule } from '@app/comon';
 import { ReservationsRepository } from './reservations.repository';
 import {
   ReservationDocument,
@@ -14,6 +14,7 @@ import {
     DatabaseModule.forFeature([
       { name: ReservationDocument.name, schema: ReservationsSchema },
     ]),
+    LoggerModule,
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
