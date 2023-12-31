cfc6efa96c3cfe0e27b90979cfe064ef004171dd
Ayaf-Bless <blessambel1@gmail.com>
2023-12-31 15:36:17 +0300

    implement billing the user

--- apps/payments/src/payments.controller.ts
+++ apps/payments/src/payments.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { MessagePattern, Payload } from '@nestjs/microservices';
-import { CreateChargeDto } from '@app/comon';
+import { CREATE_CHARGE_MESSAGE_PARTERN, CreateChargeDto } from '@app/comon';
 
 @Controller()
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
-  @MessagePattern('create-charge')
+  @MessagePattern(CREATE_CHARGE_MESSAGE_PARTERN)
   async createCharge(@Payload() data: CreateChargeDto) {
     return this.paymentsService.createCharge(data);
   }
--- libs/comon/src/constants/services.ts
+++ libs/comon/src/constants/services.ts
@@ -2,6 +2,7 @@
 export const AUTH_SERVICE = 'auth';
 export const PAYMENTS_SERVICE = 'payments';
 export const AUTHENTICATE_MESSAGE_PARTERN = 'authenticate';
+export const CREATE_CHARGE_MESSAGE_PARTERN = 'create-charge';
 
 // AUTH
 export const TCP_AUTH_PORT = 'TCP_AUTH_PORT';
