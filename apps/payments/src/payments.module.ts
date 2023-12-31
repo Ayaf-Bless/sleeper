38b9d7d79f7ba0d7b1795ff2c5c6bd6e0467dd58
Ayaf-Bless <blessambel1@gmail.com>
2023-12-31 14:18:51 +0300

    setup stripe

--- apps/payments/src/payments.module.ts
+++ apps/payments/src/payments.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from '@app/comon';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        // HTTP_PAYMENTS_PORT: Joi.number().re quired(),
+        TCP_PAYMENTS_PORT: Joi.number().required(),
+        STRIPE_SECRET_KEY: Joi.string().required(),
+      }),
+    }),
+    LoggerModule,
+  ],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
